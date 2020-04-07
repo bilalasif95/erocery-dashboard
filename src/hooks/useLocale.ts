d9bcaa7a01be06dd1c3b3bca4e59928785a4a51e
farhan <farhansaeed27@gmail.com>
2020-04-07 09:14:43 +0500

    Delted

--- src/hooks/useLocale.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { LocaleContext } from "@saleor/components/Locale";
-
-function useLocale() {
-  const localeInfo = useContext(LocaleContext);
-  return localeInfo;
-}
-export default useLocale;
--- src/hooks/useShop.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-
-import { ShopContext } from "@saleor/components/Shop";
-
-function useShop() {
-  return useContext(ShopContext);
-}
-export default useShop;
